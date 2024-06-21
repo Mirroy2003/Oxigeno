57abe8f46a09dca665ee9fc723b4832fa8a44f29
Mirroy2003 <u202121975@upc.edu.pe>
2024-06-21 07:57:10 -0500

    dsdad
    dsd

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+
+if(!navigator.geolocation){
+  alert('Navegadr no soporta la Geolacation')
+  throw new Error('Navegador no soporta la Geolacation')
+}
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
